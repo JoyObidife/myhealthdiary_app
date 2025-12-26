50182ff3882e02f59edbc652a0b644fd9f408b30
JoyObidife <obidifejoy@gmail.com>
2025-12-26 07:33:47 -0800

    26 dec

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -63,47 +63,75 @@ class _HomePageState extends State<HomePage> {
               SizedBox(height: 10),
               Column(
                 children: [
-                  Container(
-                    width: double.infinity,
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    child: GestureDetector(
-                      onTap: () {},
-                      child: Padding(
-                        padding: const EdgeInsets.only(
-                          left: 16,
-                          top: 12,
-                          bottom: 12,
-                        ),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          children: [
-                            Column(
-                              children: [
-                                CircleAvatar(
-                                  radius: 15, // adjust size as needed
-                                  backgroundColor: Colors.grey.shade200,
-                                  child: Icon(
-                                    Icons.monitor_heart,
-                                    color: Colors.blue,
-                                    size: 28,
-                                  ),
-                                ),
-                                SizedBox(height: 8),
-                                Text(
-                                  "Log Symptom",
-                                  style: TextStyle(fontSize: 15),
-                                ),
-                              ],
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
+                  _buildCardView(
+                    icon: Icons.monitor_heart,
+                    color: Colors.blue,
+                    title: "Log Symptom",
+                    onTap: () {},
                   ),
                 ],
+              ),
+              // Add Medication section
+              SizedBox(height: 8),
+              _buildCardView(
+                icon: Icons.add,
+                title: "Add Medication",
+                color: Colors.green,
+                onTap: () {},
+              ),
+
+              // Schedule Appointment Section
+              SizedBox(height: 8),
+              _buildCardView(
+                icon: Icons.event,
+                title: "Schedule Appointment",
+                color: Colors.blue,
+                onTap: () {},
+              ),
+
+              // Crisis management section
+              SizedBox(height: 8),
+              _buildCardView(
+                icon: Icons.warning,
+                title: "Crisis Management",
+                color: Colors.red,
+                onTap: () {},
+              ),
+
+               SizedBox(height: 16),
+              Text(
+                "Upcomping Events",
+                style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+              ),
+
+               // Crisis management section
+              SizedBox(height: 8),
+              _buildCardView(
+                icon: Icons.event,
+                title: "Dr Chioma Appointment",
+                 subtitle: "Today at 10:00 AM",
+                color: Colors.blue,
+                onTap: () {},
+              ),
+
+               // Therapy section
+              SizedBox(height: 8),
+              _buildCardView(
+                icon: Icons.monitor_heart,
+                title: "Therapy Session",
+                 subtitle: "Friday, feb 2",
+                color: Colors.blue,
+                onTap: () {},
+              ),
+
+               //  Pain crisis section
+              SizedBox(height: 8),
+              _buildCardView(
+                icon: Icons.warning,
+                title: "Pain Crisis",
+                 subtitle: "Monday, Jan 28",
+                color: Colors.red,
+                onTap: () {},
               ),
             ],
           ),
@@ -111,4 +139,54 @@ class _HomePageState extends State<HomePage> {
       ),
     );
   }
+
+  Container _buildCardView({
+  required IconData icon,
+  required String title,
+  String? subtitle,
+  required VoidCallback onTap,
+  required Color color, // icon color
+}) {
+  return Container(
+    width: double.infinity,
+    decoration: BoxDecoration(
+      color: Colors.white,
+      borderRadius: BorderRadius.circular(10),
+    ),
+    child: GestureDetector(
+      onTap: onTap,
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+        child: Row(
+          children: [
+            CircleAvatar(
+              radius: 20,
+              backgroundColor: Colors.grey.shade200,
+              child: Icon(icon, color: color, size: 28),
+            ),
+            SizedBox(width: 16),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text(
+                  title,
+                  style: const TextStyle(fontSize: 16, color: Colors.black),
+                ),
+                if (subtitle != null)
+                  Text(
+                    subtitle,
+                    style: TextStyle(
+                      fontSize: 14,
+                      color: Colors.grey.shade600,
+                    ),
+                  ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    ),
+  );
+}
 }
